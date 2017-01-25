333de1fc763e011017f56719e829edd49ff66fe0
jiarenlu <gunbanmi@gmail.com>
2017-01-25 22:11:37 +0800

    简单单元测试实现

--- lisp-unit.lisp
+++ lisp-unit.lisp
@@ -10,8 +10,8 @@
   (format t "~:[FAIL~;pass~] ... ~a~%" (= (+ -1 -3) -4) '(= (+ -1 -3) -4)))
 
 ;; 重构
-(defun report-result(reset form)
-  (format t "~:[FAIL~;pass~] ... ~a~%" reset form))
+(defun report-result(result form)
+  (format t "~:[FAIL~;pass~] ... ~a~%" result form))
 
 (defun test-+()
   (report-result (= (+ 1 2) 3) '(= (+ 1 2) 3))
@@ -31,11 +31,119 @@
      ,@(loop for f in forms collect `(report-result ,f ',f))))
 
 (defun test-+()
+  (let ((*test-name* 'test-*))
+    (check
+      (= (+ 1 2) 3)
+      (= (+ 1 2 3) 6)
+      (= (+ -1 -3) -5))))
+
+
+;; 修改返回值
+(defun report-result (result form)
+  (format t "~:[FAIL~;pass~] ... ~a~%" result form)
+  result)
+
+(defmacro with-gensyms ((&rest names) &body body)
+  `(let ,(loop for n in names collect `(,n (gensym)))
+     ,@body))
+
+(defmacro combine-results(&body forms)
+  (with-gensyms (result)
+    `(let ((,result t))
+       ,@(loop for f in forms collect `(unless ,f (setf ,result nil)))
+       ,result)))
+
+(defmacro check(&body forms)
+  `(combine-results
+     ,@(loop for f in forms collect `(report-result ,f ',f))))
+
+
+
+;; 更好的结果输出
+(defun test-*()
+  (let ((*test-name* 'test-+))
+    (check
+      (= (* 2 2) 4)
+      (= (* 3 5) 15))))
+
+(defun test-arithmetic()
+  (combine-results
+    (test-+)
+    (test-*)))
+
+(defvar *test-name* nil)
+
+(defun report-result (result form)
+  (format t "~:[FAIL~;pass~] ... ~a: ~a~%" result *test-name* form)
+  result)
+
+
+
+;; 抽象诞生
+(defmacro deftest (name parameters &body body)
+  `(defun ,name ,parameters
+     (let ((*test-name* ',name))
+       ,@body)))
+
+
+
+
+(defmacro deftest (name parameters &body body)
+  `(defun ,name ,parameters
+     (let ((*test-name* (append *test-name* (list ',name))))
+       ,@body)))
+
+
+
+;; 完整代码
+
+(defvar *test-name* nil)
+
+(defmacro deftest (name parameters &body body)
+  "Define a test function. Within a test function we can call
+other test functions or use 'check' to run indivdual test calss."
+  `(defun ,name ,parameters
+     (let ((*test-name* (append *test-name* (list ',name))))
+       ,@body)))
+
+(defmacro check (&body forms)
+  "Run each expression in 'form' as a test case."
+  `(combine-results
+     ,@(loop for f in forms collect `(report-result ,f ',f))))
+
+(defmacro combine-results (&body forms)
+  "Combine the results (as booleans) of evaluating 'forms' in order."
+  (with-gensyms (result)
+    `(let ((,result t))
+       ,@(loop for f  in forms collect `(unless ,f (setf ,result nil)))
+       ,result)))
+
+(defun report-result (result form)
+  "Report the results of a single test case. Called by 'check'."
+  (format t "~:[FAIL~;pass~] ... ~a: ~a~%" result *test-name* form)
+  result)
+
+
+
+;; 测试示例
+
+(deftest test-+()
   (check
     (= (+ 1 2) 3)
     (= (+ 1 2 3) 6)
     (= (+ -1 -3) -4)))
 
 
-;; 修改返回值
+(deftest test-*()
+  (check
+    (= (* 2 2) 4)
+    (= (* 3 5) 15)))
 
+
+(deftest test-arithmetic ()
+  (combine-results
+    (test-+)
+    (test-*)))
+
+(deftest test-math()
+  (test-arithmetic))
