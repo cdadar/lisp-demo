d0c84377669922250cf792f26b2b7fe9d0490301
jiarenlu <gunbanmi@gmail.com>
2017-03-19 21:27:25 +0800

    modified old file

--- binary-data.lisp
+++ binary-data.lisp
@@ -11,101 +11,128 @@
            :current-binary-object
            :*null*))
 
-;; 将一个符号转化成对应的关键字符号1
-(defun as-keyword (sym) (intern (string sym) :keyword))
+(defvar *in-progress-objects* nil)
 
-;; 接收一个define-binary-class槽描述符并返回一个 DEFCLASS 槽描述符
-(defun slot->defclass-slot (spec)
-  (let ((name (first spec)))
-    `(,name :initarg ,(as-keyword name) :accessor ,name)))
-
-
-(defmacro define-binary-class (name slots)
-  `(defclass ,name ()
-     ,(mapcar #'slot->defclass-slot slots)))
-
-
+(defconstant +null+ (code-char 0))
 
 (defgeneric read-value (type stream &key)
   (:documentation "Read a value of the given type from the stream."))
 
-
-(defun slot->read-value (spec stream)
-  (destructuring-bind (name (type &rest args)) (normalize-slot-spec spec)
-    `(setf ,name (read-value ',type ,stream ,@args))))
-
-(defun normalize-slot-spec (spec)
-  (list (first spec) (mklist (second spec))))
-
-(defun mklist (x) (if (listp  x) x (list x)))
-
-(defmacro define-binary-class (name slots)
-  (with-gensyms (typevar objectvar streamvar)
-    `(progn
-       (defclass ,name ()
-         ,(mapcar #'slot->defclass-slot slots))
-       (defmethod read-value ((,typevar (eql ',name)) ,streamvar &key)
-         (let ((,objectvar (make-instance ',name)))
-           (with-slots ,(mapcar #'first slots) ,objectvar
-             ,@ (mapcar #'(lambda (x) (slot->read-value x streamvar)) slots))
-           ,objectvar)))))
-
-
-(defgeneric wriite-value (type stream value &key)
+(defgeneric write-value (type stream value &key)
   (:documentation "Write a value as the given type to the stream."))
-
-(defun slot->write-value (spec stream)
-  (destructuring-bind (name (type &rest args)) (normalize-slot-spec spec)
-    `(wriite-value ',type ,stream ,name ,@args)))
-
-(defmacro define-binary-class (name slots)
-  (with-gensyms (typevar objectvar streamvar)
-    `(progn
-       (defclass ,name ()
-         ,(mapcar #'slot->defclass-slot slots))
-       (defmethod read-value ((,typevar (eql ',name)) ,streamvar &key)
-         (let ((,objectvar (make-instance ',name)))
-           (with-slots ,(mapcar #'first slots) ,objectvar
-             ,@ (mapcar #'(lambda (x) (slot->read-value x streamvar)) slots))
-           ,objectvar))
-       (defmethod wriite-value ((,typevar (eql ',name)) ,streamvar ,objectvar &key)
-         (with-slots ,(mapcar #'first slots) ,objectvar
-           ,@ (mapcar #'(lambda (x) (slot->write-value x streamvar)) slots))))))
-
-
-
-(macroexpand-1 '(define-binary-class id3-tag
-                 ((indentifier (is-8859-1-string :length 3))
-                  (major-version u1)
-                  (revision u1)
-                  (flags u1)
-                  (size id3-tag-size)
-                  (frames (id3-frames :tag-size size)))))
-
-;; 添加继承和标记的结构
 
 (defgeneric read-object (object stream)
   (:method-combination progn :most-specific-last)
-  (:documentation "Fill in the slots of object from stream"))
-
+  (:documentation "Fill in the slots of object from stream."))
 
 (defgeneric write-object (object stream)
   (:method-combination progn :most-specific-last)
   (:documentation "Write out the slots of object to the stream."))
 
-(defmacro define-binary-class (name superclasses slots)
+(defmethod read-value ((type symbol) stream &key)
+  (let ((object (make-instance type)))
+    (read-object object stream)
+    object))
+
+(defmethod write-value ((type symbol) stream value &key)
+  (assert (typep value type))
+  (write-object value stream))
+
+
+;;; Binary types
+
+;; 生成用来读写代表已有类的实例的值
+(defmacro define-binary-type (name (&rest args) &body spec)
+  (with-gensyms (type stream value)
+    `(progn
+       (defmethod read-value ((,type (eql ',name)) ,stream &key ,@args)
+         (declare (ignorable ,@args))
+         ,(type-reader-body spec stream))
+       (defmethod write-value ((,type (eql ',name)) ,stream ,value &key ,@args)
+         (declare (ignorable ,@args))
+         ,(type-writer-body spec stream value)))))
+
+(defun type-reader-body (spec stream)
+  (ecase (length spec)
+    (1 (destructuring-bind (type &rest args) (mklist (first spec))
+         `(read-value ',type ,stream ,@args)))
+    (2 (destructuring-bind ((in) &body body) (cdr (assoc :reader spec))
+         `(let ((,in ,stream)) ,@body)))))
+
+(defun type-writer-body (spec stream value)
+  (ecase (length spec)
+    (1 (destructuring-bind (type &rest args) (mklist (first spec))
+         `(write-value ',type ,stream ,value ,@args)))
+    (2 (destructuring-bind ((out v) &body body) (cdr (assoc :writer spec))
+         `(let ((,out ,stream) (,v ,value)) ,@body)))))
+
+
+;;; Binary classes
+
+(defmacro define-generic-binary-class (name (&rest superclasses) slots read-method)
   (with-gensyms (objectvar streamvar)
     `(progn
+       (eval-when (:compile-toplevel :load-toplevel :execute)
+         (setf (get ',name 'slots) ',(mapcar #'first slots))
+         (setf (get ',name 'superclasses) ',superclasses))
+
        (defclass ,name ,superclasses
          ,(mapcar #'slot->defclass-slot slots))
 
-       (defmethod read-object progn ((,objectvar ,name) ,streamvar)
-                  (with-slots ,(mapcar #'first slots) ,objectvar
-                    ,@ (mapcar #'(lambda (x) (slot->read-value x streamvar)) slots)))
+       ,read-method
 
        (defmethod write-object progn ((,objectvar ,name) ,streamvar)
-                  (with-slots ,(mapcar #'first slots) ,objectvar
-                    ,@ (mapcar #'(lambda (x) (slot->write-value x streamvar)) slots))))))
+                  (declare (ignorable ,streamvar))
+                  (with-slots ,(new-class-all-slots slots superclasses) ,objectvar
+                    ,@(mapcar #'(lambda (x) (slot->write-value x streamvar)) slots))))))
+
+(defmacro define-binary-class (name (&rest superclasses) slots)
+  (with-gensyms (objectvar streamvar)
+    `(define-generic-binary-class ,name ,superclasses ,slots
+                                  (defmethod read-object progn ((,objectvar ,name) ,streamvar)
+                                             (declare (ignorable ,streamvar))
+                                             (with-slots ,(new-class-all-slots slots superclasses) ,objectvar
+                                               ,@(mapcar #'(lambda (x) (slot->read-value x streamvar)) slots))))))
+
+(defmacro define-tagged-binary-class (name (&rest superclasses) slots &rest options)
+  (with-gensyms (typevar objectvar streamvar)
+    `(define-generic-binary-class ,name ,superclasses ,slots
+                                  (defmethod read-value ((,typevar (eql ',name)) ,streamvar &key)
+                                    (let* ,(mapcar #'(lambda (x) (slot->binding x streamvar)) slots)
+                                      (let ((,objectvar
+                                             (make-instance
+                                              ,@(or (cdr (assoc :dispatch options))
+                                                    (error "Must supply :disptach form."))
+                                              ,@(mapcan #'slot->keyword-arg slots))))
+                                        (read-object ,objectvar ,streamvar)
+                                        ,objectvar))))))
+
+;; 将一个符号转化成对应的关键字符号
+(defun as-keyword (sym) (intern (string sym) :keyword))
+
+(defun normalize-slot-spec (spec)
+  (list (first spec) (mklist (second spec))))
+
+(defun mklist (x) (if (listp x) x (list x)))
+
+;; 接收一个define-binary-class槽描述符并返回一个 DEFCLASS 槽描述符
+(defun slot->read-value (spec stream)
+  (destructuring-bind (name (type &rest args)) (normalize-slot-spec spec)
+    `(setf ,name (read-value ',type ,stream ,@args))))
+
+(defun slot->write-value (spec stream)
+  (destructuring-bind (name (type &rest args)) (normalize-slot-spec spec)
+    `(write-value ',type ,stream ,name ,@args)))
+
+(defun slot->binding (spec stream)
+  (destructuring-bind (name (type &rest args)) (normalize-slot-spec spec)
+    `(,name (read-value ',type ,stream ,@args))))
+
+(defun slot->keyword-arg (spec)
+  (let ((name (first spec)))
+    `(,(as-keyword name) ,name)))
+
+;;; Keeping track of inherited slots
 
 ;; 返回由一个二进制类直接定义的槽
 (defun direct-slots (name)
@@ -123,124 +150,17 @@
 
 ;;计算所有新类的槽列表
 (defun new-class-all-slots (slots superclasses)
+  "Like all slots but works while compiling a new class before slots
+and superclasses have been saved."
   (nconc (mapcan #'all-slots superclasses) (mapcar #'first slots)))
 
-(defmacro define-binary-class (name (&rest superclasses) slots)
-  (with-gensyms (objectvar streamvar)
-    `(progn
-       (eval-when (:compile-toplevel :load-toplevel :execute)
-         (setf (get ',name 'slots) ',(mapcar #'first slots))
-         (setf (get ',name 'superclasses) ',superclasses))
+;;; In progress Object stack
 
-       (defclass ,name ,superclasses
-         ,(mapcar #'slot->defclass-slot slots))
+(defun current-binary-object ()
+  (first *in-progress-objects*))
 
-       (defmethod read-object progn ((,objectvar ,name) ,streamvar)
-                  (with-slots ,(new-class-all-slots slots superclasses) ,objectvar
-                    ,@ (mapcar #'(lambda (x) (slot->read-value x streamvar)) slots)))
-
-       (defmethod write-object progn ((,objectvar ,name) ,streamvar)
-                  (with-slots ,(new-class-all-slots slots superclasses) ,objectvar
-                    ,@ (mapcar #'(lambda (x) (slot->write-value x streamvar)) slots))))))
-
-;; 带有标记的结构
-
-(defmacro define-generic-binary-class (name (&rest superclasses) slots read-method)
-  (with-gensyms (objectvar streamvar)
-    `(progn
-       (eval-when (:compile-toplevel :load-toplevel :execute)
-         (setf (get ',name 'slots) ',(mapcar #'first slots))
-         (setf (get ',name 'superclasses) ',superclasses))
-
-       (defclass ,name ,superclasses
-         ,(mapcar #'slot->defclass-slot slots))
-
-       ,read-method
-
-       (defmethod with-object progn ((,objectvar ,name) ,streamvar)
-                  (declare (ignorable ,streamvar))
-                  (with-slots ,(new-class-all-slots slots superclasses) ,objectvar
-                    ,@ (mapcar #'(lambda (x) (slot->write-value x streamvar)) slots))))))
-
-(defmacro define-binary-class (name (&rest superclasses) slots)
-  (with-gensyms (objectvar streamvar)
-    `(define-generic-binary-class ,name ,superclasses ,slots
-                                  (defmethod read-object progn ((,objectvar ,name) ,streamvar)
-                                             (declare (ignorable ,streamvar))
-                                             (with-slots ,(new-class-all-slots slots superclasses) ,objectvar
-                                               ,@ (mapcar #'(lambda (x) (slot->read-value x streamvar)) slots))))))
-
-
-(defmacro define-tagged-binary-class (name (&rest superclasses) slots &rest options)
-  (with-gensyms (typevar objectvar streamvar)
-    `(define-generic-binary-class ,name ,superclasses ,slots
-                                  (defmethod read-value ((,typevar (eql ',name)) ,streamvar &key)
-                                    (let* ,(mapcar #'(lambda (x) (slot->binding x streamvar)) slots)
-                                      (let ((,objectvar
-                                             (make-instance
-                                              ,@ (or (cdr (assoc :dispatch options))
-                                                     (error "Must supply :dispatch form."))
-                                                 ,@ (mapcan #'slot->keyword-arg slots))))
-                                        (read-object ,objectvar ,streamvar)
-                                        ,objectvar))))))
-
-
-
-(defun slot->binding (spec stream)
-  (destructuring-bind (name (type &rest args)) (normalize-slot-spec spec)
-    `(,name (read-value ',type ,stream ,@args))))
-
-(defun slot->keyword-arg (spec)
-  (let ((name (first spec)))
-    `(,(as-keyword name) ,name)))
-
-;; 生成用来读写代表已有类的实例的值
-(defmacro define-binary-type (name (&rest args) &body spec)
-  (with-gensyms (type)
-    `(progn
-       ,(destructuring-bind ((in) &body body) (rest (assoc :reader spec))
-          `(defmethod read-value ((,type (eql ',name)) ,in &key ,@args)
-             ,@body))
-
-       ,(destructuring-bind ((out value) &body body) (rest (assoc :writer spec))
-          `(defmethod write-value ((,type (eql ',name)) ,out ,value &key ,@args)
-             ,@body)))))
-
-
-(defmacro define-binary-type (name (&rest args) &body spec)
-  (ecase (length spec)
-    (1
-     (with-gensyms (type stream value)
-       (destructuring-bind (derived-from &rest derived-args) (mklist (first spec))
-         `(progn
-            (defmethod read-value ((,type (eql ',name)) ,stream &key ,@args)
-              (read-value ',derived-from ,stream ,@derived-args))
-            (defmethod write-value ((,type (eql ',name)) ,stream ,value &key ,@args)
-              (wriite-value ',derived-from ,stream ,value ,@derived-args))))))
-    (2
-     (with-gensyms (type)
-       `(progn
-          ,(destructuring-bind ((in) &body body) (rest (assoc :reader spec))
-             `(defmethod read-value ((,type (eql ',name)) ,in &key ,@args)
-                ,@body))
-          ,(destructuring-bind ((out value) &body body) (rest (assoc :writer spec))
-             `(defmethod wriite-value ((,type (eql ',name)) ,out ,value &key ,@args)
-                ,@body)))))))
-
-
-
-(define-binary-type iso-8859-1-string (length)
-  (:reader (in)
-           (let ((string (make-string length)))
-             (dotimes (i length)
-               (setf (char string i) (code-char (read-byte in))))
-             string))
-  (:writer (out string)
-           (dotimes (i length)
-             (write-byte (char-code (char string i)) out))))
-
-;; 当前对象栈
-(defvar *in-progress-objects* nil)
+(defun parent-of-type (type)
+  (find-if #'(lambda (x) (typep x type)) *in-progress-objects*))
 
 (defmethod read-object :around (object stream)
   (declare (ignore stream))
@@ -252,8 +172,3 @@
   (let ((*in-progress-objects* (cons object *in-progress-objects*)))
     (call-next-method)))
 
-
-(defun current-binary-object () (first *in-progress-objects*))
-
-(defun parent-of-type (type)
-  (find-if #'(lambda (x) (typep x type)) *in-progress-objects*))
--- file.lisp
+++ file.lisp
@@ -32,14 +32,35 @@
   (and value (not (eql value :unspecific))))
 
 ;; 测试一个路径名是否已经是目录形式
+
 (defun directory-pathname-p (p)
-  (and
-   (not (component-present-p (pathname-name p)))
-   (not (component-present-p (pathname-type p)))
-   p))
+  "Is the given pathname the name of a directory? This function can
+usefully be used to test whether a name returned by LIST-DIRECTORIES
+or passed to the function in WALK-DIRECTORY is the name of a directory
+in the file system since they always return names in `directory normal
+form'."
+  (flet ((component-present-p (value)
+           (and value (not (eql value :unspecific)))))
+    (and
+     (not (component-present-p (pathname-name p)))
+     (not (component-present-p (pathname-type p)))
+     p)))
+
+
+(defun file-pathname-p (p)
+  (unless (directory-pathname-p p) p))
+
 
 ;; 将任何路径名转换成目录形式的路径名
+
 (defun pathname-as-directory (name)
+  "Return a pathname reperesenting the given pathname in
+`directory normal form', i.e. with all the name elements in the
+directory component and NIL in the name and type components. Can
+not be used on wild pathnames because there's not portable way to
+convert wildcards in the name and type into a single directory
+component. Returns its argument if name and type are both nil or
+:unspecific."
   (let ((pathname (pathname name)))
     (when (wild-pathname-p pathname)
       (error "Can't reliably convert wild pathnames."))
@@ -47,8 +68,8 @@
         (make-pathname
          :directory (append (or (pathname-directory pathname) (list :relative))
                             (list (file-namestring pathname)))
-         :name nil
-         :type nil
+         :name      nil
+         :type      nil
          :defaults pathname)
         pathname)))
 
@@ -69,24 +90,45 @@
 
 ;; 添加多个实现的兼容
 (defun list-directory (dirname)
+  "Return a list of the contents of the directory named by dirname.
+Names of subdirectories will be returned in `directory normal
+form'. Unlike CL:DIRECTORY, LIST-DIRECTORY does not accept
+wildcard pathnames; `dirname' should simply be a pathname that
+names a directory. It can be in either file or directory form."
   (when (wild-pathname-p dirname)
     (error "Can only list concrete directory names."))
+
   (let ((wildcard (directory-wildcard dirname)))
-    ;; 在 SBCL,CMUCL和LispWorks可以返回子目录
+
     #+(or sbcl cmu lispworks)
+    ;; SBCL, CMUCL, and Lispworks return subdirectories in directory
+    ;; form just the way we want.
     (directory wildcard)
-    ;; 在openmcl 如果directory传递一个实现相关的,值为真的关键字参数:directories,那么它将返回子目录
+
     #+openmcl
+    ;; OpenMCl by default doesn't return subdirectories at all. But
+    ;; when prodded to do so with the special argument :directories,
+    ;; it returns them in directory form.
     (directory wildcard :directories t)
+
     #+allegro
+    ;; Allegro normally return directories in file form but we can
+    ;; change that with the :directories-are-files argument.
     (directory wildcard :directories-are-files nil)
+
     #+clisp
+    ;; CLISP has a particularly idiosyncratic view of things. But we
+    ;; can bludgeon even it into doing what we want.
     (nconc
+     ;; CLISP won't list files without an extension when :type is
+     ;; wild so we make a special wildcard for it.
      (directory wildcard)
+     ;; And CLISP doesn't consider subdirectories to match unless
+     ;; there is a :wild in the directory component.
      (directory (clisp-subdirectories-wildcard wildcard)))
+
     #-(or sbcl cmu lispworks openmcl allegro clisp)
-    (error "list-directory not implemented")
-    ))
+    (error "list-directory not implemented")))
 
 ;; 在clisp的directory只有传递给一个以:wild作为目录组件的最后一个元素且名字和类型组件为nil的通配路径名时,才可以返回子目录
 #+clisp
@@ -96,6 +138,8 @@
    :name nil
    :type nil
    :defaults wildcard))
+
+
 
 ;; 接受一个路径名(目录形式或文件形式),并在其代表的文件存在时返回一个等价的路径名,否则返回NIL,但如果该文件存在并且是一个目录,那么总是返回目录形式的路径名
 
@@ -119,7 +163,13 @@
   (error "list-directory not implemented."))
 
 ;; pathname-as-directory 的逆函数,它返回等价于其参数的文件形式的路径名
+
 (defun pathname-as-file (name)
+  "Return a pathname reperesenting the given pathname in `file form',
+i.e. with the name elements in the name and type component. Can't
+convert wild pathnames because of problems mapping wild directory
+component into name and type components. Returns its argument if
+it is already in file form."
   (let ((pathname (pathname name)))
     (when (wild-pathname-p pathname)
       (error "Can't reliably convert wild pathnames."))
@@ -135,14 +185,30 @@
 
 ;; 遍历目录树
 ;; 接受一个目录的名字和一个函数,并在该目录下所有文件的路径名上递归地调用该函数.接受两个关键字:directories和:test.当:directories 为真时,他将所有目录的路径名和正规文件上调用该函数;如果有:test参数,它指定另一个函数,在调用主函数之前在每一个路径名上调用该函数,主函数只有当测试参数返回真时才会被调用.:test参数的默认值是一个总是返回真的函数,它是通过调用标准函数constantly而生成的
-(defun walk-directory(dirname fn &key directories (test (constantly t)))
+
+(defun walk-directory (dirname fn &key directories (test (constantly t)))
+  "Walk a directory invoking `fn' on each pathname found. If `test' is
+supplied fn is invoked only on pathnames for which `test' returns
+true. If `directories' is t invokes `test' and `fn' on directory
+pathnames as well."
   (labels
       ((walk (name)
          (cond
            ((directory-pathname-p name)
             (when (and directories (funcall test name))
               (funcall fn name))
-            (dolist (x (list-directory name))
-              (walk x)))
+            (dolist (x (list-directory name)) (walk x)))
            ((funcall test name) (funcall fn name)))))
     (walk (pathname-as-directory dirname))))
+
+
+(defun directory-p (name)
+  "Is `name' the name of an existing directory."
+  (let ((truename (file-exists-p name)))
+    (and truename (directory-pathname-p name))))
+
+(defun file-p (name)
+  "Is `name' the name of an existing file, i.e. not a directory."
+  (let ((truename (file-exists-p name)))
+    (and truename (file-pathname-p name))))
+
